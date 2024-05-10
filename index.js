c35e88f5015db5c137b539cebef06d82a80b4fb2
defwtuhaijsok <jsjcaiydasurles@hotmail.com>
2024-05-11 03:26:09 +0700

    test project

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-function maxArea(height) {
-  let maxArea = 0;
-  let left = 0;
-  let right = height.length - 1;
-  while (left < right) {
-    maxArea = Math.max(
-      maxArea,
-      Math.min(height[left], height[right]) * (right - left),
-    );
-    if (height[left] < height[right]) left++;
-    else right--;
+function addTwoNumbers(l1, l2) {
+  const dummy = new ListNode(0);
+  let p = l1,
+    q = l2,
+    curr = dummy;
+  let carry = 0;
+  while (p !== null || q !== null) {
+    const x = p !== null ? p.val : 0;
+    const y = q !== null ? q.val : 0;
+    const sum = x + y + carry;
+    carry = Math.floor(sum / 10);
+    curr.next = new ListNode(sum % 10);
+    curr = curr.next;
+    if (p !== null) p = p.next;
+    if (q !== null) q = q.next;
   }
-  return maxArea;
+  if (carry > 0) {
+    curr.next = new ListNode(carry);
+  }
+  return dummy.next;
 }
